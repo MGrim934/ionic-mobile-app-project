6f66064f4bb764217ce3ba4d6541b38dd1f4e8eb
MGrim934 <marcus934@hotmail.com>
2016-04-07 21:15:55 +0100

    Basic archiving system, slight ui changes, moved add task option to the entry page. Will remove from filter. Some basic error handling was added to ensure empty entries dont happen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,11 @@
 angular.module('starter.controllers', [])
 
-.controller('EntryCtrl', function($scope,Tasks) {
+.controller('EntryCtrl', function($scope,Tasks,$ionicModal) {
     
     var todo={
         task: "",
-        type: "",
+        type: Tasks.taskTypes[Tasks.taskTypes.length-1].title,
+        description: "",
         due: new Date(),
         min: new Date()
        /* http://stackoverflow.com/questions/29086764/set-min-date-to-current-date-in-angularjs-input*/
@@ -12,20 +13,69 @@ angular.module('starter.controllers', [])
     $scope.todo=todo;
     //function
     function add (){
-        Tasks.addToDo(todo.task,todo.type,todo.due);
+        Tasks.addToDo(todo.task,todo.description,todo.type,todo.due);
         //passing in the local variable into the factory
         //user doesn't initially change the factory value
         //so if the whole thing explodes, nothing is permanently changed or lost
         //have to submit to actually add it and change in the factory
         //reset the local values
-         console.log(todo.task + "Type "+ todo.type+todo.due);
+        console.log(todo.task + "Type "+ todo.type+todo.due);
         todo.task="";
-        todo.type="";
-        console.log(todo.task + "Type "+ todo.type);
+        todo.description="";
+        
+       
         
     }
     $scope.add= add;
     $scope.taskTypes = Tasks.taskTypes;
+    
+    
+    $scope.newCat={
+        title: ""
+    }
+    
+    function addCat(){
+        Tasks.addCategory($scope.newCat.title);
+        $scope.closeModal();
+        //this ensures that if a user adds a task that it will be the new category they just set
+        //otherwise can result in an error
+        //i.e. an attempt to add an empty type
+        todo.type=Tasks.taskTypes[Tasks.taskTypes.length-1].title;
+        //close the modal
+        //reset the value
+        $scope.newCat.title="";
+    }
+    $scope.addCat=addCat;
+    
+    
+    
+        $ionicModal.fromTemplateUrl('templates/cat-modal.html',{
+        scope: $scope,
+        animation: 'slide-in-up'
+        
+    }).then(function(modal){
+        $scope.modal=modal
+        
+        
+    })
+    
+
+    $scope.openModal = function() {
+    $scope.modal.show();
+    
+    }
+
+    $scope.closeModal = function() {
+    $scope.modal.hide();
+      
+        
+        
+    };
+
+    $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+        
+    });
   
 })
 
@@ -34,10 +84,15 @@ angular.module('starter.controllers', [])
     //local toggles to control delete or update functions
     var toggles={
         delete: false,
-        swipe: true
+        swipe: true,
+        update:false
         
     }
     $scope.toggles=toggles;
+    
+
+    
+
     
     
     
@@ -52,7 +107,9 @@ angular.module('starter.controllers', [])
     }
     
     function archive(index){
-        Task.archiveTask(Tasks.todo.list[index]);
+        Tasks.archiveTask(Tasks.todo.list[index]);
+        //now remove the task
+        removeTask(index);
     
     
     }
@@ -65,6 +122,7 @@ angular.module('starter.controllers', [])
     
 
     $scope.update=update;
+    $scope.archive=archive;
    
     
     
@@ -115,7 +173,7 @@ angular.module('starter.controllers', [])
     }
     $scope.addCat=addCat;
     $scope.addType= function (){
-        console.log("error");
+        
         $scope.openModal();
     }
     
--- www/js/services.js
+++ www/js/services.js
@@ -2,15 +2,18 @@ angular.module('starter.services', [])
 
 .factory('Tasks', function() {
     
-    var taskTypes = {taskNames: ["Work","Personal","Groceries"]};
+ 
+    
+    var taskTypes=[
+        {title: "Work"},
+        {title: "Personal"},
+        {title: "Groceries"}
+    ]
     
     var todo = { list: [
-        {task: "Dance", type: "Work", dateSet: new Date(), due: new Date()},
+        {task: "Dance", description:"I need to dance..", type: "Work", dateSet: new Date(), due: new Date()},
                          
-                         
-                         
-                         
-                         ]}
+                 ]}
     
     var completedTasks = [
         
@@ -18,17 +21,58 @@ angular.module('starter.services', [])
     ];
     
     //function to set a task
-    function addToDo(task,type,due){
+    function addToDo(task, description, type,due){
         //set the name
-        var task = {task: task, type: type, dateSet: new Date(), due: new Date(due)};
+        var valid=validateInput(task,type);
+        if(valid==true){
+        var task = {task: task, description: description, type: type, dateSet: new Date(), due: new Date(due)};
         todo.list.push(task);
+        }else{
+            console.log("Did not pass validation");
+        }
+    }
+    
+    function validateInput(task,type){
+        //the purpose of this function is to ensure that the user has placed enough info to add a new entry
+        
+        //first check if the task has been entered
+        //this is manditory - you can't have a blank task
+        if(task==null||task==undefined||task==""){
+            console.log("error");
+            return false;
+            //can't continue as they haven't entered anything!
+        }
+        //description is allowed to be blank so no need to break
+        //now check type
+        if(type==null||type==undefined||type==""){
+            return false;
+            //needs a type
+           
+        }
+        return true;
+        //if it passed the validation check
+        //continue!
+        
+        
     }
     
     //function to allow a user to add a task category
+    
     function addCategory(task){
-        if(taskTypes.taskNames.indexOf(task)<0){
+        //Need to check to see if the object is already in the taskType array
+        //derived solution from here
+        //http://stackoverflow.com/questions/8217419/how-to-determine-if-javascript-array-contains-object
+        var found=false;
+       
+        for(var i=0;i<taskTypes.length;i++){
+            if(taskTypes[i].title==task){
+                found=true;
+                break;
+            }
+        }
+        if(found==false){
             console.log("adding");
-            taskTypes.taskNames.push(task);
+            taskTypes.push({title: task});
             
         }else{
             console.log("its already there!");
